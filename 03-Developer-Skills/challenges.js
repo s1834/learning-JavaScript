d66f599c4ad6c1ed8e2676d91c30d60519035c3f
s1834 <s18324@protonmail.com>
2023-07-07 01:58:54 +0530

    Section 5: Developer Skills & Editor Setup

---
+++ 03-Developer-Skills/challenges.js
@@ -0,0 +1,14 @@
+// Developer Skills & Editor Setup
+/*
+// Coding Challenge #1
+const printForecast = function (arr) {
+  let str = `...`;
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}oC in ${i + 1} days...`;
+  }
+
+  return str;
+};
+
+console.log(printForecast([17, 21, 23]));
+*/
