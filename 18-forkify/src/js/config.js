9682ed426d271e254a39aa6c9bd57cc3c373624b
s1834 <s18324@protonmail.com>
2024-01-27 07:18:05 +0530

    section 18: forkify app

---
+++ 18-forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = " https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "77f4a19a-18f2-4221-836f-0110f5c0fa5d";
+export const MODAL_CLOSE_SEC = 2.5;
